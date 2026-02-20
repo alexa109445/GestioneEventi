1696a69c4aad28996aa3ce32e746cbe951d26dbe
alexa109445 <alemanolache109@gmail.com>
2026-02-20 10:12:20 +0100

    Setup iniziale: connessione al DB e configurazione porta

---
+++ src/test/java/alexandramanolache/GestioneEventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package alexandramanolache.GestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
