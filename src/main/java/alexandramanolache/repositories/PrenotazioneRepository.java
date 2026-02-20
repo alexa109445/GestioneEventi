6c5b65b18a1d46848fa769d103435bdbdd0689d2
alexa109445 <alemanolache109@gmail.com>
2026-02-20 15:36:19 +0100

    PrenotazioneRepository

---
+++ src/main/java/alexandramanolache/repositories/PrenotazioneRepository.java
@@ -0,0 +1,9 @@
+package alexandramanolache.repositories;
+
+import alexandramanolache.entities.Prenotazione;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Long> {
+}
