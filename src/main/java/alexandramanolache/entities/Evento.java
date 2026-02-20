88c40c552de938ce3e5119683643410114c89765
alexa109445 <alemanolache109@gmail.com>
2026-02-20 14:58:18 +0100

    Add Evento class

---
+++ src/main/java/alexandramanolache/entities/Evento.java
@@ -0,0 +1,29 @@
+package alexandramanolache.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "eventi")
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Evento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titolo;
+    private String descrizione;
+    private LocalDate luogo;
+    private int postiTotali;
+
+    @ManyToOne
+    @JoinColumn(name = "organizzatore_id")
+    private Utenti organizzatore;
+}
